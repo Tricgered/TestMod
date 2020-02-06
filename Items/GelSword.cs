0e8335560d4dc4ce15c6322e7752458a82d43eb4
ash7on <50225550+ash7on@users.noreply.github.com>
2020-02-06 03:27:51 -0800

    STRRRRF

---
+++ Items/GelSword.cs
@@ -0,0 +1,53 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace TestMod.Items
+{
+	public class GelSword : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("GelSword"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+			Tooltip.SetDefault("Wow thats slimey.");
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 8;
+			item.melee = true;
+			item.width = 40;
+			item.height = 40;
+			item.useTime = 25;
+			item.useAnimation = 25;
+			item.useStyle = 1;
+			item.knockBack = 3;
+			item.value = 100;
+			item.rare = 2;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+			//item.scale = Main.rand.Next(1,3);
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Wood, 15);
+			recipe.AddIngredient(ItemID.Gel, 5);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+
+		public override void MeleeEffects(Player player, Rectangle hitbox)
+		{
+			if (Main.rand.NextBool(7))
+			{
+				//Emit dusts when swing the sword
+				Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, 99, 0f, 0f, 60, default(Color), 1.5f);
+			}
+		}
+	}
+}

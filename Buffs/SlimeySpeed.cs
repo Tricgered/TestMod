9ca7cc6bbcd817188069ce95a690c6195873113c
ash7on <50225550+ash7on@users.noreply.github.com>
2020-02-07 02:30:35 -0800

    GelSword

---
+++ Buffs/SlimeySpeed.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace TestMod.Buffs
+{
+    public class SlimeySpeed : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("SlimeySpeed");
+            Description.SetDefault("35% MORE SPEED!");
+            Main.buffNoTimeDisplay[Type] = false;
+            Main.debuff[Type] = false; //Add this so the nurse doesn't remove the buff when healing
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.moveSpeed += .35f;
+
+        }
+    }
+}

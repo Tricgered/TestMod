2e6675048badd902d238a0a435c4c81ca1d83171
ash7on <50225550+ash7on@users.noreply.github.com>
2020-02-09 00:01:37 -0800

    STRF

---
+++ Items/GelSpear.cs
@@ -0,0 +1,54 @@
+﻿using TestMod.Projectiles;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace TestMod.Items
+{
+	public class GelSpear : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Slimey Lance");
+			Tooltip.SetDefault("Its a slimey Lance");
+		}
+
+		public override void SetDefaults() {
+			item.damage = 10;
+			item.useStyle = 5;
+			item.useAnimation = 33;
+			item.useTime = 33;
+			item.shootSpeed = 2f;
+			item.knockBack = 6.5f;
+			item.width = 32;
+			item.height = 32;
+			item.scale = 1f;
+			item.rare = 5;
+			item.value = Item.sellPrice(silver: 10);
+
+			item.melee = true;
+			item.noMelee = true; // Important because the spear is actually a projectile instead of an item. This prevents the melee hitbox of this item.
+			item.noUseGraphic = true; // Important, it's kind of wired if people see two spears at one time. This prevents the melee animation of this item.
+			item.autoReuse = true; // Most spears don't autoReuse, but it's possible when used in conjunction with CanUseItem()
+
+			item.UseSound = SoundID.Item1;
+			item.shoot = ProjectileType<GelSpearProjectile>();
+		}
+
+		
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Wood, 25);
+			recipe.AddIngredient(ItemID.Gel, 8);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+
+		public override bool CanUseItem(Player player) {
+			// Ensures no more than one spear can be thrown out, use this when using autoReuse
+			return player.ownedProjectileCounts[item.shoot] < 1;
+		}
+	}
+}
